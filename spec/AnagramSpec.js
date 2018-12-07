4174e14fd9c40a2b3beffe6fb2d2de94220c73b5
Lucien Najev <luciennajev@hotmail.com>
2018-12-07 15:47:33 +0000

    feature: getAnagrams takes account of hyphens

--- spec/AnagramSpec.js
+++ spec/AnagramSpec.js
@@ -6,16 +6,19 @@ describe ('Anagram', () => {
   let anagram2;
   let anagram3;
   let anagram4;
+  let anagram5;
   let data = ['paste', 'pates', 'peats', 'septa', 'spate', 'tapes', 'tepas', 'zzzzz', 'moreletters'];
   let data2 = ['Paste', 'PATES', 'Peats', 'SEPTA', 'Spate', 'Tapes', 'Tepas', 'Zzzzz', 'MorEletters'];
   let data3 = ['AA'];
   let data4 = ["Malin's", "Malebolge's", "Paron's", "Reynaldo's"]
+  let data5 = ["Mal-in's", "Male-bolge's", "Par-on's", "Reynaldo's"]
 
   beforeEach(() => {
     anagram = new Anagram(data);
     anagram2 = new Anagram(data2);
     anagram3 = new Anagram(data3);
     anagram4 = new Anagram(data4);
+    anagram5 = new Anagram(data5);
   });
 
   it('shows all the anagrams of a sample word', () => {
@@ -35,5 +38,11 @@ describe ('Anagram', () => {
   });
   it('takes account of apostrophes when input word contains one', () => {
     expect(anagram4.getAnagrams("snila'm")).toEqual("snila'm has the following anagrams: Malin's");
+  });
+  it('takes account of hyphens', () => {
+    expect(anagram5.getAnagrams("snilam")).toEqual("snilam has the following anagrams: Mal-in's");
+  });
+  it('takes account of hyphens when input word contains one', () => {
+    expect(anagram5.getAnagrams("sni-lam")).toEqual("sni-lam has the following anagrams: Mal-in's");
   });
 });
--- src/Anagram.js
+++ src/Anagram.js
@@ -6,13 +6,13 @@ function Anagram(data) {
 
 Anagram.prototype.getAnagrams= function(word) {
   let result = [];
-  let sortedWord = word.replace(/'/g, '').toLowerCase().split('').sort().join('');
+  let sortedWord = word.replace(/['-]/g, '').toLowerCase().split('').sort().join('');
 
   console.log(sortedWord);
   console.log(this.data);
 
   for (let i = 0; i < this.data.length; i +=1) {
-    if (this.data[i].replace(/'/g, '').toLowerCase().split('').sort().join('') === sortedWord && this.data[i].toLowerCase() !== word.toLowerCase()) {
+    if (this.data[i].replace(/['-]/g, '').toLowerCase().split('').sort().join('') === sortedWord && this.data[i].toLowerCase() !== word.toLowerCase()) {
       result.push(this.data[i]);
     }
   }
